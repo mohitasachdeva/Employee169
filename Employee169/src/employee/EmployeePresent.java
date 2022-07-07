c1c401a1799908417ff54d1be68883d6a152cf8f
HP <sachdevamohita5@gmail.com>
2022-07-07 21:54:34 +0530

    [mohita] employee is present or absent

--- Employee169/src/employee/EmployeePresent.java
+++ Employee169/src/employee/EmployeePresent.java
@@ -1,4 +1,12 @@
 package employee;
 
 public class EmployeePresent {
+    public static void main(String[] args) {
+        int IS_FULL_TIME=1;
+        double empCheck=Math.floor(Math.random()*10%2);
+        if (empCheck==IS_FULL_TIME)
+            System.out.println("employee is present");
+        else
+            System.out.println("employee is absent");
+    }
 }
