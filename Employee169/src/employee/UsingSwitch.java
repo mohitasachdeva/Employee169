77a803ce255f26d5519af8e64fac6bf14b5fbee7
HP <sachdevamohita5@gmail.com>
2022-07-08 14:16:16 +0530

    [mohita] usih switch case

---
+++ Employee169/src/employee/UsingSwitch.java
@@ -0,0 +1,29 @@
+package employee;
+
+public class UsingSwitch {
+    public static final int IS_FULL_TIME = 2;
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_EMP_RATE_PER_HRS = 20;
+
+    public static void main(String[] args) {
+        int empHrs = 0;
+        int empWage = 0;
+        int empcheck = (int) Math.floor(Math.random() * 10) % 3;
+        switch (empcheck) {
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            default:
+                empHrs = 0;
+        }
+
+        empWage = empHrs * IS_EMP_RATE_PER_HRS;
+        System.out.println("emp wage:" + empWage);
+    }
+
+}
+
+
